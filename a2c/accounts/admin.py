2098c0724f9ceccc79d757cf996d6b7271aa36d6
johnlizenith <info@zen-tec.us>
2014-09-15 04:52:18 +0000

    add accounts to south

--- a2c/accounts/admin.py
+++ a2c/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from accounts.models import Contact, CreditCard
 
-# Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+class CreditCardAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Contact,ContactAdmin)
+admin.site.register(CreditCard,CreditCardAdmin)
