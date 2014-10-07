96fc919b389c9a72cb21b1e8967d1eaf33892f50
johnlizenith <info@zen-tec.us>
2014-10-07 02:32:40 +0000

    reflux index

--- a2c/plans/conf.py
+++
@@ -1,8 +0,0 @@
-
-# TODO use django-conf
-from django.conf import settings
-
-TAX =  getattr(settings, 'PLANS_TAX', None)
-TAXATION_POLICY =  getattr(settings,
-                           'PLANS_TAXATION_POLICY',
-                           'plans.taxation.TAXATION_POLICY')
